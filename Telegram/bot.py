e36e653a82cc3e1eb33723e3764ecee629e35967
Vivek Tp <Vivektvp1@gmail.com>
2023-03-09 14:48:46 +0530

    Update bot.py

--- Telegram/bot.py
+++ Telegram/bot.py
@@ -7,13 +7,19 @@ print("Initializing MongoDB client")
 mongo_client = MongoClient(config.MONGO_URL)
 db = mongo_client.groupmanager
 app = Client("Client",api_id = config.API_ID,api_hash = config.API_HASH,bot_token = config.BOT_TOKEN,plugins={"root": "Telegram/plugins"})
+BOT_USERNAME = 
 
-START_BTNS = InlineKeyboardButton(
+
+START_BTNS = InlineKeyboardMarkup(
+    [
+        InlineKeyboardButton(text="help", url="https://t.me/{BOT_USERNAME}?start=help_")
+        ]
+    )
 
 @app.on_message(filters.command("start"))
 async def start_handler(_, message):
     await app.send_message(
         chat_id = message.chat.id,
-        text = "Hey there! I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential."
+        text = "Hey there! I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.",
         reply_markup=START_BTNS
         )
