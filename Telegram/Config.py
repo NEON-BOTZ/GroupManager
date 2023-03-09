9459044ae3ca46167aa3969a9ebdbc5b12b2710b
Vivek Tp <Vivektvp1@gmail.com>
2023-03-09 13:38:49 +0530

    Update Config.py

--- Telegram/Config.py
+++ Telegram/Config.py
@@ -1,8 +1,8 @@
 class config:
-    API_ID = int()
-    API_HASH = ""
-    BOT_TOKEN = ""
-    BOT_ID = ''
+    API_ID = int(2454821)
+    API_HASH = "7081c3faf0347270c2bd0b614dfbfb8c"
+    BOT_TOKEN = "6065018011:AAHDN798ypAw8t_efmyLNTLNmveAHgTwVro"
+    BOT_ID = '6065018011'
     MONGO_URL = 'mongodb+srv://KoichiGrey:KoichiGrey@cluster0.2bzkqov.mongodb.net/?retryWrites=true&w=majority'
-    DEV = [1244977099,1467358214]
+    DEV = [1244977099,1467358214,6290455685]
     OPEN_AI_KEY = ''
