53af4ed5e76b2288c64891a5c1c26ece75653ee5
Vivek Tp <Vivektvp1@gmail.com>
2023-03-09 15:11:04 +0530

    Update bot.py

--- Telegram/bot.py
+++ Telegram/bot.py
@@ -10,16 +10,14 @@ app = Client("Client",api_id = config.API_ID,api_hash = config.API_HASH,bot_toke
 BOT_USERNAME = "testdeluxrobot"
 
 
-START_BTNS = InlineKeyboardMarkup(
-    [
-        InlineKeyboardButton(text="help", url="https://t.me/{BOT_USERNAME}?start=help_")
-    ]
-)
-
 @app.on_message(filters.command("start"))
 async def start_handler(_, message):
     await app.send_message(
         chat_id = message.chat.id,
         text = "Hey there! I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.",
-        reply_markup=START_BTNS
+        reply_markup=InlineKeyboardMarkup(
+            [
+                InlineKeyboardButton(text="Help", url="http://t.me/testdeluxrobot?start=help_")
+                ]
+            )
         )
